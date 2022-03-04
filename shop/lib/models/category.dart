76dfa93d302214a32b38f075743cbe00c47a6357
user <brigas29@gmail.com>
2022-03-04 19:56:30 +0700

    test1

---
+++ shop/lib/models/category.dart
@@ -0,0 +1,9 @@
+class Category {
+  final String icon, title;
+  Category({required this.icon, required this.title});
+}
+
+List<Category> demo categories = [
+Category(icon: "assets/icons/dress.svg", title: "Dress",),
+
+];
